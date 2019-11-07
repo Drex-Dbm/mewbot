624ef9500c6abfaa8b718dd33d58cdfb85722384
godboyxd <47825463+godboyxd@users.noreply.github.com>
2019-11-07 06:15:46 -0500

    Update mewbot.py

--- mewbot.py
+++ mewbot.py
@@ -2427,4 +2427,4 @@ async def on_message(message):
     await ctx.send(f"Your {pn} has evolved into a {evo}!")
     await bot.db.release(pconn)
 
-    await client.login('NDY5MTc2NDgwMDQ4MzQ5MjA2.XcP8eA.tdocSz0ZM_whelWWGQdw9y4sSEY')
+    client.login('NDY5MTc2NDgwMDQ4MzQ5MjA2.XcP8eA.tdocSz0ZM_whelWWGQdw9y4sSEY')
